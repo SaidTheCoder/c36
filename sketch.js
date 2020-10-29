a07189d666d146c6313a47044e3f314c24acc4c9
SaidTheCoder <71198810+SaidTheCoder@users.noreply.github.com>
2020-10-29 19:50:42 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var backgrounImage,gameState=0,playerCount,database,form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  createCanvas(400,400);
+game = new Game();
+game.getState();
+game.start();
+}
+
+function draw(){
+  background("white");
+  
+   
+    drawSprites();
+  
+}
